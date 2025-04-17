5e4e11674a046ad1cff77bdc405f44086c20da95
pavanvikas1 <pavang@jfrog.com>
2025-04-17 13:34:27 +0530

    Update Multi1.java

--- multi1/src/main/java/artifactory/test/Multi1.java
+++ multi1/src/main/java/artifactory/test/Multi1.java
@@ -3,8 +3,9 @@ package artifactory.test;
 /**
  * Hello world! testing
  */
+
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! --  Password=123**");
     }
 }
