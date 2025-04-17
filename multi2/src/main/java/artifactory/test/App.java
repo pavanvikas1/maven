16f7015db7773ccbe49a80e7156e963bd078a34a
pavanvikas1 <pavang@jfrog.com>
2025-04-17 19:14:31 +0530

    Update App.java

--- multi2/src/main/java/artifactory/test/App.java
+++ multi2/src/main/java/artifactory/test/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        string token="eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWNTYiLCJraWQiOiJseHU3SVZFS0Ryb0JDcXZwTjNhczhHN1IyRUljbk1SR0xmWVA1MFRQbkFvIn0.eyJzdWIiOiJqZmFjQDAxamN4NnE5bnB0enp0MGQzcmFteWswOXM3L3VzZXJzL3VtYWMiLCJzY3AiOiJhcHBsaWVkLXBlcm1pc3Npb25zL2FkbWluIiwiYXVkIjoiKkAqIiwiaXNzIjoiamZmZUAwMWpjeDZxOW5wdHp6dDBkM3JhbXlrMDlzNyIsImlhdCI6MTc0NDg3NzE4NSwianRpIjoiNDgxNDE4MjYtNjE3NS00ZWRiLWFlNjItNWExZjU1Y2FjNzZiIiwidGlkIjoiYTBjdmxjY2Mya2xyMyJ9.FAA7wC9V-emYWI1kmXNygHEazNKL5XC2jECTIRSCVp0KtBBO2znOs-gp9PI7vAm8lTyYYioKl9nik5z3399O_BWfGzvmf4Hdv-X7-jKfqNGqyysgIle9wAhMsruUIphGS2AyHLcA_qCh9oajwy0ArwSb9sG4fCeavapz2cS7xwfsfBRFu6GWXYaFw92DgbNjR4jFHyp6th-F_-pAL41kNolvyZuzc-DnfZs8pO5yOoPNqOQRkhgNX5CpwIX10IzWZ_8GaeFkVnBbStLDS1fVdje36rmE4yX5fXYRJICUzIZVOtNVNvmnngZw5EW8OA7FcdOpmIHztMByynw0tKbsRQ";
     }
 }
